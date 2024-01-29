39bbc36c3dc1e11819f4e3b72560d81195f20de7
10Zeeko <57009593+10Zeeko@users.noreply.github.com>
2024-01-29 21:21:35 +0100

    Improvements in grappling behavior

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -7,13 +7,34 @@ const JUMP_VELOCITY = -400.0
 const GRAVITY = 50
 const FRICTION_AIR = 0.95
 const FRICTION_GROUND = 0.85
+const ARM_PULL = 100
 
+var arm_velocity = Vector2(0, 0)
 var can_jump = false
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			$Arm.shoot(event.position - get_viewport().size * 0.5)
+		else:
+			$Arm.release()
 
 func _physics_process(delta):
 	var move = (Input.get_action_strength("right") - Input.get_action_strength("left")) * SPEED
 	
 	velocity.y += GRAVITY
+	
+	if $Arm.hooked:
+		arm_velocity = to_local($Arm.tip).normalized() * ARM_PULL
+		if arm_velocity.y > 0:
+			arm_velocity.y *= 0.55
+		else:
+			arm_velocity.y *= 1.65
+		if sign(arm_velocity.x) != sign(move):
+			arm_velocity *= 0.7
+	else:
+		arm_velocity = Vector2(0, 0)
+	velocity += arm_velocity
 	
 	velocity.x += move
 	move_and_slide()
@@ -31,8 +52,6 @@ func _physics_process(delta):
 		velocity.y = -5
 		
 	if Input.is_action_just_pressed("jump"):
-		print ("TONTO")
-		print (grounded)
-		print (can_jump)
 		if grounded and can_jump:
+			can_jump = false
 			velocity.y = JUMP_VELOCITY
---
+++ Scripts/Player/arm.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@onready var rope = $Rope
+var direction := Vector2(0, 0)
+var tip := Vector2(0, 0)
+
+const SPEED = 50
+
+var flying = false
+var hooked = false
+
+func shoot(dir: Vector2):
+	direction = dir.normalized()
+	flying = true
+	tip = self.global_position
+
+func release():
+	flying = false
+	hooked = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta) -> void:
+	self.visible = flying or hooked
+	if not self.visible:
+		return
+	var tip_loc = to_local(tip)
+	$Tip.rotation = self.position.angle_to_point(tip_loc) - deg_to_rad(90)
+	rope.set_point_position(0, Vector2(0, 0))
+	rope.set_point_position(1, tip_loc)
+	
+func _physics_process(delta):
+	$Tip.global_position = tip
+	if flying:
+		if $Tip.move_and_collide(direction * SPEED):
+			hooked = true
+			flying = false
+		tip = $Tip.global_position
